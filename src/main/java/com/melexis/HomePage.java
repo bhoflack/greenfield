23d644cf6968fa64776d4409145453234fd03246
Brecht Hoflack <brh@melexis.com>
2010-12-14 15:59:09 +0100

    Initial version

---
+++ src/main/java/com/melexis/HomePage.java
@@ -0,0 +1,37 @@
+package com.melexis;
+
+import java.util.Date;
+
+import org.apache.wicket.PageParameters;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.WebPage;
+
+import org.apache.wicket.RequestCycle;
+import org.apache.wicket.markup.html.link.Link;
+
+/**
+ * Homepage
+ */
+public class HomePage extends WebPage {
+
+	private static final long serialVersionUID = 1L;
+
+	// TODO Add any page properties or variables here
+
+    /**
+	 * Constructor that is invoked when page is invoked without a session.
+	 *
+	 * @param parameters
+	 *            Page parameters
+	 */
+    public HomePage(final PageParameters parameters) {
+
+        // Add the simplest type of label
+        add(new Label("message", "If you see this message wicket is properly configured and running"));
+        add(new Link("page") {
+                public void onClick() {
+                    setResponsePage(ParameterPage.newInstance("brecht", new Date()));
+                }
+            });
+    }
+}
---
+++ src/main/java/com/melexis/ParameterPage.java
@@ -0,0 +1,43 @@
+package com.melexis;
+
+import java.util.Date;
+import java.util.HashMap;
+
+import org.apache.wicket.PageParameters;
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+
+public final class ParameterPage extends WebPage {
+
+    /**
+     * This factory should be used by other classes.
+     * @param name the person's name.
+     * @param today the date today.
+     **/
+    public final static ParameterPage newInstance(final String name, final Date today) {
+        final HashMap<String, Object> parameters = new HashMap<String, Object>();
+        parameters.put("name", name);
+        parameters.put("date", today.toString());
+
+        return new ParameterPage(new PageParameters(parameters));
+    }
+
+    /**
+     * This class accepts the following parameters:
+     * name: a string containing the person's name.
+     * date: a date containing the current date.
+     * other parameters are ignored.
+     * When none of these parameters are provided a RuntimeException
+     * is thrown.
+     **/
+    public ParameterPage(final PageParameters pp) {
+        if (!pp.containsKey("name") ||
+            !pp.containsKey("date")) {
+            throw new RuntimeException("This page requires parameters name and date.");
+        }
+
+        add(new Label("name", pp.getString("name")));
+        add(new Label("date", pp.getString("date")));
+    }
+
+}
