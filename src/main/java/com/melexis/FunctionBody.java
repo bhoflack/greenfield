1f15a664287fffa2f2ec25e772b66340087d7e7e
Brecht Hoflack <brh@melexis.com>
2010-12-15 17:18:19 +0100

    Created initial interface for FunctionBody.

---
+++ src/main/java/com/melexis/FunctionBody.java
@@ -0,0 +1,12 @@
+package com.melexis;
+
+public interface FunctionBody<T> {
+
+    /**
+     * Invoke a pure(-ish) function.
+     * @param entity the entity to work on
+     * @return the updated entity
+     **/
+    T invoke(T entity);
+
+}
